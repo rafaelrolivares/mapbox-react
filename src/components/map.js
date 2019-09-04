109d7c026036fb76f215916ac8bcc061702c000a
rafaelrolivares <rafaelrolivares@gmail.com>
2019-09-04 15:55:07 +0200

    disabled map rotation

--- src/components/map.js
+++ src/components/map.js
@@ -7,6 +7,8 @@ import { config } from './config.js'
 
 mapboxgl.accessToken = config.mapbox_key
 
+
+
 export class Map extends React.Component {
   
   componentDidUpdate() {
@@ -17,22 +19,37 @@ export class Map extends React.Component {
   componentDidMount() {
     console.log("componentDidMount")
 
+    // Limit the map to be seen only in the Netherlands
+    // const boundBox = [
+    //   [3.294817, 50.697401],
+    //   [7.382357, 53.704082] 
+    // ]
+
     // Load mapbox map:
     this.map = new mapboxgl.Map({
       container: this.mapContainer,
       style: 'mapbox://styles/mapbox/streets-v9',
       // zoom to the NL:
       // center: [5.25, 52.23],
-      // zoom: 7
+      // zoom: 7,
+      // maxBounds: boundBox
       center: [5, 34],
       zoom: 1.5
     });
 
+    // Disable rotation and other funny behaviours not really useful here.
+    this.map.dragRotate.disable()
+
     // Add navigation control buttons
-    this.map.addControl(new mapboxgl.NavigationControl(), 'top-right')
+    const navBar = new mapboxgl.NavigationControl({
+      showCompass: false,
+      showZoom: true
+    })
+    this.map.addControl(navBar, 'top-right')
+    console.log(navBar)
     
     // Add drawing tool buttons
-    const mapDraw = new MapboxDraw({
+    const Draw = new MapboxDraw({
       displayControlsDefault: false,
       controls: {
       point: true, 
@@ -42,7 +59,7 @@ export class Map extends React.Component {
       }
       });
 
-    this.map.addControl(mapDraw, 'top-right')
+    this.map.addControl(Draw, 'top-right')
 
     // Add full screen button
     this.map.addControl(new mapboxgl.FullscreenControl({container: document.querySelector('body')}));
@@ -59,15 +76,20 @@ export class Map extends React.Component {
         id: 'countries',
         type: 'fill',
         source: 'countries'
-      }, 'country-label-lg'); // ID matches `mapbox/streets-v9`
+      }, 'country-label-lg');
 
-      this.setFill();
+     this.setFill();
     });
+
+  
   }
+
+
 
   // establish how to colour the polygons based on the classification, which is described on the reducer.
   setFill() {
     const { property, stops } = this.props.active;
+    console.log(this.props)
     this.map.setPaintProperty('countries', 'fill-color', {
       property,
       stops
